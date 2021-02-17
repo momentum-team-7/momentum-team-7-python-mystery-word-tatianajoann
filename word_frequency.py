1ab9185270d8dbaa202c4ae7b50a4811166a652d
tatiana perez <tjperez@ncsu.edu>
2021-02-16 21:30:27 -0500

    no

--- word_frequency.py
+++ word_frequency.py
@@ -1,13 +1,39 @@
-STOP_WORDS = [
+stop_words = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
-    'will', 'with'
+    'will', 'with', ''
 ]
+stop_characters = [";", ":", "?", "!", ",", ".", '"', "'"]
 
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    with open(file, "r") as open_file:
+        read_file = open_file.read()
+        # print(type(read_file))
+        for i in stop_characters:
+            read_file = read_file.replace(i, '')
+        read_file = read_file.replace("\n", ' ')
+        read_file = read_file.replace("—", ' ')
+        read_file = read_file.replace("-", ' ')
+        read_file = read_file.lower()
+        read_file = read_file.split(" ")
+        read_file.remove('a')
+        # print(read_file)
+        copy_list = []
+        for word in read_file:
+            # print(word)
+            # print(read_file)
+            if word not in stop_words:
+                copy_list.append(word)
+        # print(read_file)
+        # print(copy_list)
+        another_list = []
+        for word in copy_list:
+            if word not in another_list:
+                print(word, "|", copy_list.count(word),
+                      "*" * copy_list.count(word))
+                another_list.append(word)
 
 
 if __name__ == "__main__":
@@ -25,3 +51,13 @@ if __name__ == "__main__":
     else:
         print(f"{file} does not exist!")
         exit(1)
+
+# read in file and print out the frequency of words in that file
+# open file and save it to a variable - jupyter 09
+# remove punctuation
+# normalize all words to lowercase
+# remove stop words, words used so frequently theyre ignored
+# check each word and see if its equal to a stop word , if it is
+# conditional with if
+# go through file word by word and keep a count of how often each word is ued.
+# probably use a dictionary to store this key => words, values => occurences
